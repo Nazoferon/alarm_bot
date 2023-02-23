5fe8a7f599a1fa43df490c453e25c00e770f67dc
Nazar <76437962+Nazoferon@users.noreply.github.com>
2023-02-23 13:07:40 +0200

    Add files via upload

---
+++ alarm.py
@@ -0,0 +1,40 @@
+from telethon import TelegramClient, events  # Імпортуємо потрібні бібліотеки
+import aiogram
+from aiogram import Bot, Dispatcher, types, executor
+
+# підключаємо бота
+bot = Bot(token='6196748470:AAHB2V3Ron3PozsGZzaVjITCbppHwUPDTZA')
+dp = Dispatcher(bot)
+ 
+api_id = 27076955  # Вводимо id нашого телеграм клієнта, та записуємо номер щоб не загубити 
+api_hash = 'e6a998a44112d921c71b7b6665807b25'  # Вводимо hash нашого телеграм клієнта
+
+chat_bezbab = -727502806
+
+ 
+client = TelegramClient("Test", api_id, api_hash)  # Збираемо клієнта до купи
+target_can = -1001844129402  # Вводимо id в який будемо пересилати повідомлення
+key_words_onalarm = ["Повітряна тривога в Волинська область", "Повітряна тривога в Рівненська область"] # Вводимо ключові слова які будемо шукати в повідомленнях
+key_wordss_offalarm = ["Відбій тривоги в Волинська область", "Відбій тривоги в Рівненська область"]
+#key_words_onalarm = ["Повітряна тривога"]
+#key_wordss_offalarm = ["Відбій тривоги"]
+ 
+@client.on(events.NewMessage(
+    chats=[-1001766138888, -1001802336341]))  # Запускаємо наш клієнт та скануемо на які саме канали реагувати
+async def normal_handler(event):  # Обробляємо подію
+    for i in range(len(key_words_onalarm or key_wordss_offalarm)):  # Перебираємо всі ключові слова з нашого списку
+        if key_words_onalarm[i] in event.message.message:  # Перевіряємо кожне слово на наявність його в нашому повідомленні
+            print(event.message.peer_id, event.message.message)  # Роздруковуемо в консоль id чату/групи та текст знайденного повідомлення (не обов'язково)
+            # await client.send_file(target_can, 'https://i.gifer.com/origin/4e/4e3431b51b9a7587feade70da83f2216.gif')
+            # await client.send_message(target_can, event.message)   # Пересилаємо знайдене повідомлення
+            await bot.send_message(chat_id=chat_bezbab, text=event.message.text)
+            break
+        if key_wordss_offalarm[i] in event.message.message:  # Перевіряємо кожне слово на наявність його в нашому повідомленні
+            print(event.message.peer_id, event.message.message)  # Роздруковуемо в консоль id чату/групи та текст знайденного повідомлення (не обов'язково)
+            # await client.send_file(target_can, 'https://media.tenor.com/cKrq7zekafEAAAAC/відбійповітряноїтривоги-їжак.gif')
+            # await client.send_message(target_can, event.message)
+            await bot.send_message(chat_id=chat_bezbab, text=event.message.text)
+            break
+ 
+client.start()  # Запускаємо кліент
+client.run_until_disconnected()  # Ставимо його в бескінечний цикл
