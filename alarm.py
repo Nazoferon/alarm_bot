a4fc8491e6ad8c5c2d4e45adae03cc64589b1eeb
BunnyB <76437962+Nazoferon@users.noreply.github.com>
2025-03-30 16:51:43 +0300

    Update alarm.py

--- alarm.py
+++ alarm.py
@@ -4,18 +4,18 @@ from aiogram import Bot, Dispatcher, types, executor
 from datetime import datetime
 
 # підключаємо бота
-bot = Bot(token='6215212505:AAFjRsEP7YbpKEC_OPQZFbRH3gD-dGe3UyA')
+bot = Bot(token='')
 dp = Dispatcher(bot)
  
 api_id = 27076955  # Вводимо id нашого телеграм клієнта, та записуємо номер щоб не загубити 
-api_hash = 'e6a998a44112d921c71b7b6665807b25'  # Вводимо hash нашого телеграм клієнта
+api_hash = ''  # Вводимо hash нашого телеграм клієнта
 
-chat_bezbab = -727502806
-chat_test = 5900123750
+chat1 = ID
+chat_test = ID
 
  
 client = TelegramClient("Test", api_id, api_hash)  # Збираемо клієнта до купи
-target_can = -1001844129402  # Вводимо id в який будемо пересилати повідомлення
+target_can = ID  # Вводимо id в який будемо пересилати повідомлення
 key_words_onalarm = ["Повітряна тривога в Волинська область"] # Вводимо ключові слова які будемо шукати в повідомленнях
 key_wordss_offalarm = ["Відбій тривоги в Волинська область"]
 #key_words_onalarm = ["Повітряна тривога"]
@@ -31,16 +31,16 @@ async def normal_handler(event):  # Обробляємо подію
             print('\n================================================================================\n')
             # await client.send_file(target_can, 'https://i.gifer.com/origin/4e/4e3431b51b9a7587feade70da83f2216.gif')
             # await client.send_message(target_can, event.message)   # Пересилаємо знайдене повідомлення
-            await bot.send_animation(chat_id=chat_bezbab, animation='https://i.gifer.com/origin/4e/4e3431b51b9a7587feade70da83f2216.gif')
-            await bot.send_message(chat_id=chat_bezbab, text=event.message.text)
+            await bot.send_animation(chat_id=chat1, animation='https://i.gifer.com/origin/4e/4e3431b51b9a7587feade70da83f2216.gif')
+            await bot.send_message(chat_id=chat1, text=event.message.text)
             
         elif key_wordss_offalarm[i] in event.message.message:  # Перевіряємо кожне слово на наявність його в нашому повідомленні
             print(f"Data: {datetime.now().strftime('%d.%m.%Y %T')}\nID Group: {event.message.peer_id} | Text: {event.message.message}") # Роздруковуемо в консоль id чату/групи та текст знайденного повідомлення (не обов'язково)
             print('\n================================================================================\n')
             # await client.send_file(target_can, 'https://media.tenor.com/cKrq7zekafEAAAAC/відбійповітряноїтривоги-їжак.gif')
             # await client.send_message(target_can, event.message)
-            await bot.send_animation(chat_id=chat_bezbab, animation='https://media.tenor.com/cKrq7zekafEAAAAC/відбійповітряноїтривоги-їжак.gif')
-            await bot.send_message(chat_id=chat_bezbab, text=event.message.text)
+            await bot.send_animation(chat_id=chat1, animation='https://media.tenor.com/cKrq7zekafEAAAAC/відбійповітряноїтривоги-їжак.gif')
+            await bot.send_message(chat_id=chat1, text=event.message.text)
             
  
 client.start()  # Запускаємо кліент
